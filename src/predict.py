7e719a09f983d5b7cd017601cc7c64fd9abada3d
a.gazzaev <a.gazzaev@mts.ai>
2024-04-28 13:57:50 +0300

    add predict.py

---
+++ src/predict.py
@@ -0,0 +1,22 @@
+import pickle
+from skimage import io
+from skimage.transform import resize
+from skimage.color import rgb2gray
+
+
+neigh = pickle.load(open('knnpickle_file', 'rb'))
+
+def predict_image(file_path):
+    image = io.imread(file_path)
+    if len(image.shape) == 4:
+        image = image.squeeze(0)
+    image = rgb2gray(resize(image, (200,200)))
+    image = image.reshape(1, -1)
+
+    res = neigh.predict(image)[0]
+    if res == 0:
+        print("Cat")
+    else:
+        print("Dog")
+
+predict_image("../data/PetImages/cat/3004.jpg")
