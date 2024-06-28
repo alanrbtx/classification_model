5eb7de89a841afa436e6ce9599b62fb1fd682d86
a.gazzaev <a.gazzaev@mts.ai>
2024-06-28 21:35:00 +0300

    update

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,12 +1,14 @@
 import requests
 
+
 res = requests.get("http://127.0.0.1:8000/get_test_prediction")
+print(res)
 res.json().get("result")
 print("--------------------------------")
 print("TEST ENDPOINT RESULT: ",  res.json().get("result"))
 
 url = 'http://127.0.0.1:8000/get_real_prediction'
-files = {'media': open('../data/PetImages/Cat/3004.jpg', 'rb')}
+files = {'media': open('data/PetImages/Cat/3004.jpg', 'rb')}
 res = requests.post(url, files=files)
 res.json().get("result")
 print("--------------------------------")
