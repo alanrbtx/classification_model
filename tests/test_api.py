00424d215477661e72d35f38700598b5d3076c35
a.gazzaev <a.gazzaev@mts.ai>
2024-06-28 20:56:37 +0300

    update

--- tests/test_api.py
+++ tests/test_api.py
@@ -6,7 +6,7 @@ print("--------------------------------")
 print("TEST ENDPOINT RESULT: ",  res.json().get("result"))
 
 url = 'http://127.0.0.1:8000/get_real_prediction'
-files = {'media': open('/Users/a.gazzaev/Desktop/BIG-DATA/lab1/classification_model/data/PetImages/Cat/3004.jpg', 'rb')}
+files = {'media': open('../data/PetImages/Cat/3004.jpg', 'rb')}
 res = requests.post(url, files=files)
 res.json().get("result")
 print("--------------------------------")
